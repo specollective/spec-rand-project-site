3a46dbb28f33ddbdc756e3186c28ffc59e0fbe03
Victoria <victoriakarenlo@gmail.com>
2022-08-22 16:08:30 -0700

    Add Links for page routes

--- pages/projects/by-the-numbers.js
+++ pages/projects/by-the-numbers.js
@@ -1,7 +1,13 @@
+import Link from "next/link"
+
 export default function ByTheNumbers(){
     return (
     <div className="bg-spec-blue-600 text-white">
-        <h1 className="text-4xl font-bold">By the Numbers</h1>
+        <h1 className="text-4xl font-bold">
+            <Link href="/by-the-numbers">
+                <a>By the Numbers</a>
+            </Link>
+        </h1>
         <div className="grid grid-cols-2 grid-rows-2">
             <div>Percentage picture</div>
             <div>Graph Picture</div>
