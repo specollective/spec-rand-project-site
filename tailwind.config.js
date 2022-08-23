7d205e20738f52134ccc5cf6efa37eaee319f67a
Crystal Shamsi <crystallynnvoss@gmail.com>
2022-08-23 10:12:28 -0600

    Adding Images & Adjusting Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,11 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.{html,js}', 
+    './node_modules/tw-elements/dist/js/**/*.js'],
+  plugins: [
+    require('tw-elements/dist/plugin')
   ],
-  // content: ['./src/**/*.{html,js}', './node_modules/tw-elements/dist/js/**/*.js'],
-  // plugins: [
-  //   require('tw-elements/dist/plugin')
-  // ],
   theme: {
     extend: {
       colors: {
