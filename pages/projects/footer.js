ae61a9ad2b621888a33430ff6cfff62b9d584988
Victoria <victoriakarenlo@gmail.com>
2022-08-25 17:59:31 -0700

    Update footer links

--- pages/projects/footer.js
+++ pages/projects/footer.js
@@ -1,20 +1,70 @@
 import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Image from "next/image";
+import facebook from '../../public/facebook.svg';
+import twitter from '../../public/twitter.svg';
+import linkedin from '../../public/linkedin.svg';
+import github from '../../public/github.svg';
+import specLogo from '../../public/spec-logo.svg';
+import Link from 'next/link'
 
 export default function Footer() {
   return (
-    <footer className="bg-spec-yellow-600">
-      <a
-        href="https://www.specollective.org/"
-        target="_blank"
-      >
-        <img src="/spec-logo.svg" alt="Spec Logo" className="logo" />
-      </a>
-      <div className={`flex ml-auto space-x-3`}>
-          <div className="link">About Us <span><FontAwesomeIcon icon={faCaretDown} /></span></div>
-          <div className="link">Docs <span><FontAwesomeIcon icon={faCaretDown} /></span></div>
-          <div className="link">Partnerships <span><FontAwesomeIcon icon={faCaretDown} /></span></div>
-          <div className="link">Get Involved <span><FontAwesomeIcon icon={faCaretDown} /></span></div>
+    <footer className="flex h-270 bg-spec-yellow-600 p-10">
+      <div className="w-1/3 grid place-items-end pr-10">
+        <div className="mr-5">
+          <Image
+            src={specLogo}  
+            alt="spec-logo"
+            className=""
+            width={130}
+            height={130}
+          />
+        </div>
+        <div className="grid grid-cols-4 gap-1">
+          <Link href="https://www.facebook.com/specollective">
+            <Image
+              src={facebook}
+              alt="facebook"
+              className=""
+            />
+          </Link>
+          <Link href="https://twitter.com/specollective">
+            <Image
+              src={twitter}
+              alt="twitter"
+              className=""
+            />
+          </Link>
+          <Link href="https://linkedin.com/company/specollective/">
+            <Image
+              src={linkedin}
+              alt="linkedin"
+              className=""
+            />
+          </Link>
+          <Link href="https://github.com/specollective">
+            <Image
+              src={github}
+              alt="github"
+              className=""
+            />
+          </Link>
+        </div>
+      </div>
+      <div class="w-2/3 grid content-center pl-5">
+        <div className="grid grid-rows-3 grid-cols-3 leading-10">
+          <div><Link href="/">About Us</Link></div>
+          <div><Link href="/">Blog</Link></div>
+          <div><Link href="/">Terms of Use</Link></div>
+          <div><Link href="/">Contact Us</Link></div>
+          <div><Link href="/">JoER</Link></div>
+          <div><Link href="/">Code of Conduct</Link></div>
+          <div><Link href="/">Get Involved</Link></div>
+          <div><Link href="/">Documentation</Link></div>
+          <div><Link href="/">Privacy Policy</Link></div>
+        </div>
+        <div className="font-bold mt-10">© 2022 Sustailable Progress & Equality Collective </div>
       </div>
     </footer>
   )
