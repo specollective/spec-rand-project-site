c96ef75baae0c9c9c796c47deb58455dff93b0b7
Crystal Shamsi <84194997+crystallynnvoss@users.noreply.github.com>
2022-09-02 18:14:38 -0600

    image bubble component (#36)
    * Remove css import from index
    
    * Add projects images
    
    * Implement Image Bubbles

---
+++ components/image-bubbles.js
@@ -0,0 +1,26 @@
+import Image from "next/future/image";
+import { Children } from "react";
+
+export default function ImageBubbles({ children, width=66, height=33 }) {
+
+    const arrayChildren =  Children.toArray(children).map((element,index) => {
+    const left = index * Math.floor(width * .9);
+    
+    return <Image src={`${element.props.src}`} 
+              key={index}
+              width={width} 
+              height={height} 
+              layout="raw" 
+              alt={element.props.alt}
+              className={`absolute`}
+              style={{left:`${left}px`}} 
+              />
+  })   
+  return(
+    <div className="flex flex-row p-0 relative">
+      {arrayChildren}
+    </div>
+  )
+}
+
+
