3324ae3884e21cc47948b0ab1f49800b11af1fb2
Crystal Shamsi <crystallynnvoss@gmail.com>
2022-09-06 12:42:37 -0600

    Delete margins in grid container component

--- components/grid-container.js
+++ components/grid-container.js
@@ -1,6 +1,6 @@
 export default function GridContainer({ children, className="grid-rows-1" }) {
   return(
-    <div className={`grid grid-cols-2 ${className} mt-10 mb-10`}>
+    <div className={`grid grid-cols-2 ${className}`}>
       { children }
     </div>
   )
