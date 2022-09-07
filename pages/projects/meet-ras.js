578132c70f92b0a897e5fa1ff74399f406b653d7
Victoria <victoriakarenlo@gmail.com>
2022-09-06 17:24:53 -0700

    Modify carousel

--- pages/projects/meet-ras.js
+++ pages/projects/meet-ras.js
@@ -3,8 +3,11 @@ import SectionHeader from "../../components/section-header";
 import SectionContent from "../../components/section-content";
 import SubHeader from "../../components/sub-header";
 import TextContent from "../../components/text";
-import quoteImage from "../../public/nan_sm_bubble.svg";
-import GridContainer from "../../components/grid-container";
+import nan from "../../public/nan_sm_bubble.svg";
+import alicia from "../../public/alicia_sm_bubble.svg";
+import yoline from "../../public/yoline_sm_bubble.svg";
+import crystal from "../../public/crystal_sm_bubble.svg";
+import victoria from "../../public/victoria_sm_bubble.svg";
 
 
 export default function MeetRAs() {
@@ -12,48 +15,56 @@ export default function MeetRAs() {
         <SectionContent className="bg-spec-teal-600 pb-24">
             <SectionHeader>SPEC / CAREP Research Associates (RA) </SectionHeader>
             <SubHeader>CAREP Pillar III - Policy Leadership</SubHeader>
-            <TextContent>
+            <TextContent className="pb-10">
                 The SPEC / CAREP Research associates used open collaboration throughout the project-building process and are excited to continue to share knowledge, and resources, and collaboratively innovate to create systemic change.
             </TextContent>
 
-            <div id="carouselExampleControls" className="carousel slide relative pt-10" data-bs-ride="carousel" data-bs-interval="false">
+            <div id="carouselExampleControls" className="carousel slide relative" data-bs-ride="carousel" data-bs-interval="false">
                 <div className="carousel-inner relative w-full overflow-hidden">
                     <div className="carousel-item active relative float-left w-full">
                         <div className="grid grid-cols-4">
                             <div className="pr-10">
                                 <Image
-                                    src={quoteImage}
-                                    alt="yellow-quote"
+                                    src={nan}
+                                    alt="nan-mead"
+                                    width={150}
+                                    height={150}
                                 />
-                                <TextContent className="">
+                                <TextContent className="text-black font-bold">
                                     Nan Elieen Mead
                                 </TextContent>
                             </div>
                             <div className="pr-10">
                                 <Image
-                                    src={quoteImage}
-                                    alt="yellow-quote"
+                                    src={alicia}
+                                    alt="alicia-bong"
+                                    width={150}
+                                    height={150}
                                 />
-                                <TextContent className="italic">
-                                    Nan Eileen Mead
+                                <TextContent className="text-black font-bold">
+                                    Alicia Bong
                                 </TextContent>
                             </div>
                             <div className="pr-10">
                                 <Image
-                                    src={quoteImage}
-                                    alt="yellow-quote"
+                                    src={yoline}
+                                    alt="yoline-bangerjee"
+                                    width={150}
+                                    height={150}
                                 />
-                                <TextContent className="">
-                                    Nan Elieen Mead
+                                <TextContent className="text-black font-bold">
+                                    Yoline Banerjee
                                 </TextContent>
                             </div>
                             <div className="pr-10">
                                 <Image
-                                    src={quoteImage}
-                                    alt="yellow-quote"
+                                    src={victoria}
+                                    alt="victoria-lo"
+                                    width={150}
+                                    height={150}
                                 />
-                                <TextContent className="">
-                                    Nan Elieen Mead
+                                <TextContent className="text-black font-bold">
+                                    Victoria Lo
                                 </TextContent>
                             </div>
                         </div>
@@ -63,20 +74,24 @@ export default function MeetRAs() {
                         <div className="grid grid-cols-4">
                             <div className="pr-10">
                                 <Image
-                                    src={quoteImage}
-                                    alt="yellow-quote"
+                                    src={alicia}
+                                    alt="alicia-bong"
+                                    width={150}
+                                    height={150}
                                 />
-                                <TextContent className="italic">
-                                Sagittis viverra integer et mauris, sapien enim. Eget quis congue suspendisse elit. Nisi bibendum nisl diam nulla velit, magna eget tincidunt ornare. Et integer rhoncus, magnis quisque ut sollicitudin quam mauris aenean. Penatibus purus nec duis quam integer scelerisque erat placerat in. A vitae ornare pretium id egestas. Sagittis in arcu sed sit. Viverra amet ut mus phasellus odio.
+                                <TextContent className="">
+                                    Alicia Bong
                                 </TextContent>
                             </div>
                             <div className="pr-10">
                                 <Image
-                                    src={quoteImage}
+                                    src={nan}
                                     alt="yellow-quote"
+                                    width={150}
+                                    height={150}
                                 />
                                 <TextContent className="italic">
-                                    Commodo duis sit hendrerit malesuada morbi ut. Quis orci blandit amet eget etiam vitae enim. Aliquet purus massa egestas natoque pharetra venenatis. Auctor vel ultrices tincidunt sed eget elementum id a lectus. Consequat aliquet nibh tellus nulla tempor. Viverra tempor, etiam bibendum at massa placerat.
+                                    Test
                                 </TextContent>
                             </div>
                         </div>
@@ -86,27 +101,31 @@ export default function MeetRAs() {
                         <div className="grid grid-cols-4">
                             <div className="pr-10">
                                 <Image
-                                    src={quoteImage}
+                                    src={nan}
                                     alt="yellow-quote"
+                                    width={150}
+                                    height={150}
                                 />
                                 <TextContent className="italic">
-                                Sagittis viverra integer et mauris, sapien enim. Eget quis congue suspendisse elit. Nisi bibendum nisl diam nulla velit, magna eget tincidunt ornare. Et integer rhoncus, magnis quisque ut sollicitudin quam mauris aenean. Penatibus purus nec duis quam integer scelerisque erat placerat in. A vitae ornare pretium id egestas. Sagittis in arcu sed sit. Viverra amet ut mus phasellus odio.
+                                    Test
                                 </TextContent>
                             </div>
                             <div className="pr-10">
                                 <Image
-                                    src={quoteImage}
+                                    src={nan}
                                     alt="yellow-quote"
+                                    width={150}
+                                    height={150}
                                 />
                                 <TextContent className="italic">
-                                    Commodo duis sit hendrerit malesuada morbi ut. Quis orci blandit amet eget etiam vitae enim. Aliquet purus massa egestas natoque pharetra venenatis. Auctor vel ultrices tincidunt sed eget elementum id a lectus. Consequat aliquet nibh tellus nulla tempor. Viverra tempor, etiam bibendum at massa placerat.
+                                    Test
                                 </TextContent>
                             </div>
                         </div>
                     </div>
                     </div>
                 <button
-                    className="carousel-control-prev absolute top-30 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+                    className="carousel-control-prev top-30 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
                     type="button"
                     data-bs-target="#carouselExampleControls"
                     data-bs-slide="prev"
