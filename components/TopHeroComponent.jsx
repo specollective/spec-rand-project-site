7fb38e9bb6d46583db568b1ce40f0d1ae43c0cd5
Saige Cross <saigecrossdev@gmail.com>
2022-09-22 14:50:19 -0500

    Hero finished.

--- components/TopHeroComponent.jsx
+++ components/TopHeroComponent.jsx
@@ -5,19 +5,19 @@ import RAND from '../public/@RAND.svg'
 
 function TopHeroComponent() {
   return (
-    <div className="bg-hero-img bg-cover xs:h-[625px] sm:h-[550px] bg-center text-center font-Poppins md:pt-4 lg:pt-10">
+    <div className="bg-hero-img bg-cover xs:h-[625px] sm:h-[550px] md:pt-4 lg:pt-10 bg-center text-center font-Poppins">
       <div className='xs:px-10 xs:pt-10 lg:px-20'>
-        <div className=''>
+        <div>
           <Image className='object-scale-down' src={SpecCollab} alt="logo" />
         </div>
-        <h2 className='text-white xs:text-2xl xs:px-10 lg:text-5xl font-bold lg:pb-4'>Center to Advance Racial Equity Policy </h2>
-        <div className='text-right xs:pb-2 xs:pr-12 lg:pr-44 lg:pb-8 xl:px-96'>
+        <h2 className='xs:text-2xl xs:px-10 lg:text-5xl lg:pb-4 font-bold text-white'>Center to Advance Racial Equity Policy </h2>
+        <div className='text-right xs:pb-2 xs:pr-12 lg:pr-44 xl:px-60 2xl:pr-96 lg:pb-8'>
           <Image className='object-scale-down' src={RAND} alt="randLogo" />
         </div>
-        <div className='text-white xs:pb-4 lg:pb-8 md:text-xl xl:px-60'>
+        <div className='xs:pb-4 md:text-xl lg:pb-8 xl:px-40 2xl:px-60 text-white'>
           <p>In February 2022, Sustainable Progress and Equality Collective (SPEC) announced a partnership with the RAND Center to Advance Racial Equity Policy to create and implement CAREP’s flagship Microcredential program. </p>
         </div>
-        <div className='text-white md:text-xl xl:px-60'>
+        <div className='md:text-xl xl:px-40 2xl:px-60 text-white'>
           <p>A special class of SPEC/RAND Research Associates was comprised of a diverse group of individuals with experience in academic, healthcare, corporate, government, and nonprofit settings to gain in-depth experience in research, educational technology, and organizational development. </p>
         </div>
       </div>
