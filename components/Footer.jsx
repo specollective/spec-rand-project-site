15d9a5fa888e3a71fd9d48006c9bbf4889f95ec8
Saige Cross <saigecrossdev@gmail.com>
2022-09-22 11:08:36 -0500

    Spacing finished, adding links.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,34 +6,34 @@ function Footer() {
   return (
     <div className='h-72 w-screen bg-spec-yellow-600'>
       <div className='flex justify-evenly'>
-        <div className='pt-14 bg-slate-100'>
-          <div className='block'>
-            <Image className='h-16 w-16' src={SpecLogo} />
+        <div className='pt-14'>
+          <div className='text-center'>
+            <Image className='place-items-center' src={SpecLogo} />
           </div>
           <a className='p-2'>social</a>
           <a className='p-2'>social</a>
           <a className='p-2'>social</a>
           <a className='p-2'>social</a>
         </div>
-        <div className='h-72 space-y-10 pt-6'>
+        <div className='h-60 space-y-10 pt-6'>
           <a className='block'>About Us</a>
           <a className='block'>Contact Us</a>
           <a className='block'>Get Involved</a>
         </div>
-        <div className='h-72 space-y-10 pt-6'>
+        <div className='h-60 space-y-10 pt-6'>
           <a className='block'>Blog</a>
           <a className='block'>JoER Us</a>
           <a className='block'>Documentation</a>
         </div>
-        <div className='h-72 space-y-10 pt-6 bg-slate-400'>
+        <div className='h-60 space-y-10 pt-6'>
           <a className='block'>Terms of Use</a>
           <a className='block'>Code of Conduct</a>
           <a className='block'>Privacy Policy</a>
         </div>
       </div>
-      <div>
-        <span>© 2022 Sustailable Progress & Equality Collective </span>
-      </div>
+        <div className='text-center'>
+          <p className='block'>© 2022 Sustailable Progress & Equality Collective </p>
+        </div>
     </div>
   )
 }
