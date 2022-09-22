671f95ed645c0757770cb2b2ab254bd74684b2e3
Victoria <victoriakarenlo@gmail.com>
2022-09-22 11:02:28 -0700

    Initial commit

---
+++ components/stat-box.js
@@ -0,0 +1,19 @@
+const statInfo = [
+    { borderColor: "spec-yellow-600", 
+    height: 100, 
+    width: 100, 
+    statColor: "spec-teal-600", 
+    inputStat: "", 
+    description: ""
+
+    }
+]
+
+export default function statBox({borderColor, height, width, statColor, inputStat, description}) {
+    return (
+        <div>
+
+        </div>
+
+    )
+}
