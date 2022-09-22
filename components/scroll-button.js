5516370009c091cb1a7fe3a8d607187dc4791039
Yoline <92892101+YolineB@users.noreply.github.com>
2022-09-22 11:53:23 -0700

    Add scroll to top functionality

--- components/scroll-button.js
+++ components/scroll-button.js
@@ -1,7 +1,27 @@
+import { useState } from 'react';
 import Image from "next/image";
-import tempBtn from "../public/spec_logo.svg"
+import tempBtn from "../public/spec_logo.svg";
 
 export default function ScrollButton () {
+  
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    
+    if (scrolled > 300) {
+      setVisible(true)
+    } else {
+      setVisible(false)
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top:0,
+      behavior: 'smooth'
+    })
+  }
   
   const btnStyle = `
       fixed
@@ -9,14 +29,12 @@ export default function ScrollButton () {
       top-200
       z-1
   `
-  
-  const clickAction = () => {
-    alert("I AM BUTTON")
-  }
+
+  window.addEventListener('scroll', toggleVisible);
 
   return (
     <>
-      <btn onClick={clickAction} className={btnStyle}>
+      <btn onClick={scrollToTop} className={btnStyle}>
         <Image src={tempBtn} alt="spec-logo"/>
       </btn>
     </>
