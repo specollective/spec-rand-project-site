5308b55cd2d389362038a1ac4277ce001da94447
Victoria <victoriakarenlo@gmail.com>
2022-09-22 13:09:41 -0700

    Add styles for statbox

--- components/stat-box.js
+++ components/stat-box.js
@@ -1,19 +1,23 @@
 const statInfo = [
     { borderColor: "spec-yellow-600", 
     height: 100, 
-    width: 100, 
+    width: "w-80", 
     statColor: "spec-teal-600", 
-    inputStat: "", 
-    description: ""
+    inputStat: "6", 
+    description: "Research Associates (RAs) worked on this project"
 
     }
 ]
 
-export default function statBox({borderColor, height, width, statColor, inputStat, description}) {
+export default function StatBox({borderColor, height, width, statColor, inputStat, description}) {
     return (
-        <div>
-
+        <div className="">
+            <div className={`grid border border-${borderColor} ${height} ${width} place-content-center text-center p-4`}>
+                <p className={`text-${statColor} text-7xl font-semibold`}>{inputStat}</p>
+                <p className="">
+                    {description}
+                </p>
+            </div>
         </div>
-
     )
 }
