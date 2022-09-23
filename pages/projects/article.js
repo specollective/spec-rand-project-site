52a4fcc66bf246dead32d7e23ad81069e384a957
Victoria <victoriakarenlo@gmail.com>
2022-09-23 12:34:58 -0700

    Adjust margin and grid

--- pages/projects/article.js
+++ pages/projects/article.js
@@ -4,14 +4,15 @@ import wordCloud from '../../public/article_word_cloud.svg'
 
 export default function Article() {
     return (
-      <div className="flex flex-wrap gap-4 bg-spec-teal-600 p-10 md:p-20">
-        <div className="flex-none text-5xl text-spec-yellow-600 font-bold space-y-5 md:ml-20 md:px-30">
+      <div className="gap-4 bg-spec-teal-600 p-10 md:p-20">
+        <div className="grid grid-cols-1 grid-rows-1 md:grid-cols-2">
+        <div className="text-5xl text-spec-yellow-600 font-bold space-y-5 pb-10 md:ml-20 md:pl-30">
           <p>Research</p>
           <p>Methods</p> 
           <p>Article</p>
         </div>
-        <div className="flex-1 md:ml-20 md:mr-20">
-          <p className="text-spec-yellow-600 text-xl tracking-wide font-medium pb-2">
+        <div className="md:mr-20">
+          <p className="text-spec-yellow-600 text-xl tracking-wide font-medium pb-5 md:pb-0">
             CAREP Pillar I - Methods and Action
           </p>
           <p className="text-white">
@@ -23,6 +24,8 @@ export default function Article() {
 
             <p className="font-bold pt-10">Release Date: October 31, 2022</p>
           </p>
+        </div>
+
         </div>
 
         <div>
