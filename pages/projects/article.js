a461c977fc4f3ccffd3a08b4ae5daa3842692d92
Victoria <victoriakarenlo@gmail.com>
2022-09-22 17:11:37 -0700

    Update article section

--- pages/projects/article.js
+++ pages/projects/article.js
@@ -1,30 +1,33 @@
 import Image from "next/image"
 import wordCloud from '../../public/article_word_cloud.svg'
-import Link from 'next/link'
-import SectionContent from '../../components/section-content'
-import SectionHeader from '../../components/section-header'
-import SubHeader from '../../components/sub-header'
+
 
 export default function Article() {
     return (
-        <>
-            <SectionContent className="bg-spec-teal-600">
-                <SectionHeader className="">Article</SectionHeader>
-                <SubHeader >CAERP Pillar I - Methods and Action</SubHeader>
-                    <textContent>
-                        Morbi facilisis semper ornare nunc. Amet aenean porttitor et est auctor proin. Elementum ut gravida aliquet nibh orci, sodales. Sit volutpat aliquet tincidunt vestibulum in. Mauris, diam sed consectetur non hendrerit et. Aliquet ac ullamcorper ultrices facilisi. Dolor, diam duis porta leo nunc malesuada mattis euismod augue. Augue malesuada adipiscing elit at quisque. Mattis facilisis lectus justo, nibh consequat tortor vel amet lectus. Magna tristique adipiscing felis ante nisl. Id adipiscing nisi netus purus mi facilisi. Purus sed viverra neque, turpis bibendum diam pharetra lacus dui.
-                    </textContent >
-                    <p className="text-med font-bold text-right underline">
-                        <Link href="/full-article">
-                            <a>See full article</a>
-                        </Link>
-                    </p>
-                <Image
-                    src={wordCloud}
-                    alt="word-cloud"
-                    className=""
-                />
-            </SectionContent>
-        </>
-    )
+      <div className="flex flex-wrap gap-4 bg-spec-teal-600 p-20">
+        <div className="flex-none text-5xl text-spec-yellow-600 font-bold ml-20 px-30 space-y-5">
+          <p>Research</p>
+          <p>Methods</p> 
+          <p>Article</p>
+        </div>
+        <div className="flex-1 ml-20 mr-20">
+          <p className="text-spec-yellow-600 text-xl tracking-wide font-medium pb-2">
+            CAREP Pillar I - Methods and Action
+          </p>
+          <p className="text-white">
+            The world of work has become more global in nature as technology 
+            enables us to interact with individuals from different cultures, 
+            religions and life experiences. How does intercultural competence 
+            impact one’s ability to communicate and collaborate in the 
+            twenty-first-century marketplace?
+
+            <p className="font-bold pt-10">Release Date: October 31, 2022</p>
+          </p>
+        </div>
+
+        <div>
+            <Image src={wordCloud} alt="word-cloud" className="" />
+        </div>
+      </div>
+    );
 }
