af74a1f26c5fa1174f2cd3618fa8aaaed92ad035
Yoline <92892101+YolineB@users.noreply.github.com>
2022-09-23 11:50:31 -0700

    Reconfigure lineWidth and lineHeight props

--- components/break-line.js
+++ components/break-line.js
@@ -1,12 +1,12 @@
-export default function BreakLine({ color="white", lineWidth="w-1/3" , lineHeight="h-1"}) {
+export default function BreakLine({ color="white", lineWidth="1/3" , lineHeight="1"}) {
   
   return (
     <hr 
       className={`
         my-6
         mx-auto 
-        ${lineWidth}
-        ${lineHeight}
+        w-${lineWidth}
+        h-${lineHeight}
         bg-${color} 
         rounded 
         border-0
