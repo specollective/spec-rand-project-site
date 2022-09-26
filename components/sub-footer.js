82b806faafd53a54997af3f591ba29d07a2c2e73
Crystal Shamsi <84194997+crystallynnvoss@users.noreply.github.com>
2022-09-26 08:44:23 -0600

    Update components/sub-footer.js
    Co-authored-by: Jack Kinsey <jack.w.kinsey@gmail.com>

--- components/sub-footer.js
+++ components/sub-footer.js
@@ -3,7 +3,7 @@ export default function Subfooter({
 }) {
   return (
     <div
-      className={`${className} flex justify-center p-20 md:p-10 px-4 text-center text-sm text-slate-100 font-medium`}
+      className={`bg-${color} flex justify-center p-20 md:p-10 px-4 text-center text-sm text-slate-100 font-medium`}
     >
       Funding for this research was provided by unrestricted gifts from RAND
       supporters and income from operations, with which RAND established the
