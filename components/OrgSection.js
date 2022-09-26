b681b5fd337cc8bbc60f9e087a1b5e8adc543832
Saige Cross <saigecrossdev@gmail.com>
2022-09-26 08:20:36 -0500

    WIP PR set up.

---
+++ components/OrgSection.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function OrgSection() {
+  return (
+    <div className='w-screen h-12 bg-slate-300'>
+      OrgSection
+    </div>
+  )
+}
+
+export default OrgSection
