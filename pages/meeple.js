ee32d7fbd69cc615384cbea3b4cb488fb9f8865e
Saige Cross <saigecrossdev@gmail.com>
2022-09-26 12:56:29 -0500

    project overview good

--- pages/meeple.js
+++ pages/meeple.js
@@ -14,9 +14,16 @@ const MeeplePage = () => {
       {/* <div>
         BACK TO HOME LINK
       </div> */}
-      
+
       <div className="w-screen h-auto p-12 bg-spec-black-600 text-center">
-        <Image src={MeepleMetaverse} alt="meeple hero" />
+        <div className="py-8 lg:py-32">
+          <Image src={MeepleMetaverse} alt="meeple hero" />
+        </div>
+
+        <div className="md:grid md:grid-cols-2 lg:px-24 xl:px-60 text-left">
+          <h1 className="pb-8 px-8 xs:text-center md:text-left xs:text-3xl sm:text-3xl md:text-4xl lg:text-5xl text-spec-yellow-600">Project Overview</h1>
+          <p>The aim of this project, funded by RAND's Center to Advance Racial Equity Policy, is to engage and educate the public on topics related to racial equity policy through socio-cultural game-based learning This involves developing a site that will host original, web-based minigames and interactives highlighting key learnings from RAND racial equity research.</p>
+        </div>
       </div>
 
 
