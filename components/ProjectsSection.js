4ad4fe07d4e81dd7134e23f998a3a1980fde9b70
Yoline <92892101+YolineB@users.noreply.github.com>
2022-09-26 12:33:29 -0700

    Add section description

--- components/ProjectsSection.js
+++ components/ProjectsSection.js
@@ -7,6 +7,7 @@ function ProjectsSection() {
     <>
       <div className="p-5 md:px-36 w-screen h-auto bg-spec-black-600">
         <h1 className="text-4xl md:text-6xl font-bold py-4 text-center"> Projects </h1>
+        
         <section className="flex flex-col md:flex-row gap-y-3 py-4 text-spec-yellow-600 text-xl tracking-wide font-medium justify-around items-center"> 
           <div>CAREP Pillar I - Methods and Action</div>
           <span className="hidden md:block rounded-full bg-spec-white-600 h-1.5 w-1.5"></span>
@@ -14,6 +15,14 @@ function ProjectsSection() {
           <span className="hidden md:block rounded-full bg-spec-white-600 h-1.5 w-1.5"></span>
           <div>CAREP Pillar III - Policy Leadership</div>
         </section>
+        
+        <p className="py-4 md:px-44  text-lg text-center">
+        Embodying all three pillars while moving through the project phase allowed both the SPEC and CAREP 
+        teams to learn and grow while creating tangible 
+        deliverables which will accelerate learning and up-skilling in pertinent knowledge areas. 
+        </p>
+
+        
       </div>
     </>
   )
