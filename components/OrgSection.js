661354fb181860f41ba0590782d5057a91a3e614
Saige Cross <saigecrossdev@gmail.com>
2022-09-26 09:12:21 -0500

    colors corrected, everything finished on mobile.

--- components/OrgSection.js
+++ components/OrgSection.js
@@ -2,40 +2,30 @@ import React from 'react'
 import Image from 'next/image'
 import SpecLogo from '../public/spec_logo.svg'
 import RacialEquity from '../public/racialequityimg.svg'
+import SpecWhite from '../public/specwhite.svg'
+import PillarOne from '../public/pillar1.svg'
+import PillarTwo from '../public/pillar2.svg'
+import PillarThree from '../public/pillar3.svg'
 
 function OrgSection() {
 
-  // const socialLinkData = [
-  //   { site: 'facebook', url: 'https://www.facebook.com/specollective', imageSrc: Facebook },
-  //   { site: 'twitter', url: 'https://twitter.com/specollective', imageSrc: Twitter },
-  //   // etc
-  // ]
-
-  // const socialLinks = socialLinkData.map(link => {
-  //   return (
-  //     <div className='inline p-1' key={link.site}>
-  //       <a href={link.url}><Image src={link.imageSrc} /></a>
-  //     </div>
-  //   );
-  // });
-
   const specData = [
-    { key:'sustainability', imageSrc: SpecLogo, header: 'Sustainability through Systems Thinking', textBody: 'Using critical awareness when building systems, seeking solutions that usher in improvement locally and globally and applying a variety of research and development methods.'},
-    { key:'progress', imageSrc: SpecLogo, header: 'Progress through Open Collaboration ', textBody: 'Striving for openess in our research and communications. Seeking to understand the world by trying to create transformative change through collaboration, research and critical reflection.'},
-    { key:'equality', imageSrc: SpecLogo, header: 'Equality through Mutual Reciprocity', textBody: 'Treat everyone in our communities with compassion, mutual respect and accountability. Equality is the end goal; diversity, equity, inclusion and reciprocity are the means to get there.'}
+    { key:'sustainability', imageSrc: SpecWhite, header: 'Sustainability through Systems Thinking', textBody: 'Using critical awareness when building systems, seeking solutions that usher in improvement locally and globally and applying a variety of research and development methods.'},
+    { key:'progress', imageSrc: SpecWhite, header: 'Progress through Open Collaboration ', textBody: 'Striving for openess in our research and communications. Seeking to understand the world by trying to create transformative change through collaboration, research and critical reflection.'},
+    { key:'equality', imageSrc: SpecWhite, header: 'Equality through Mutual Reciprocity', textBody: 'Treat everyone in our communities with compassion, mutual respect and accountability. Equality is the end goal; diversity, equity, inclusion and reciprocity are the means to get there.'}
   ]
 
   const policyData = [
-    { key:'1', imageSrc: SpecLogo, header: 'Methods and action', textBody: 'Identifying the most effective combination of approaches that can have enduring impact on systemic racism.' },
-    { key:'2', imageSrc: SpecLogo, header: 'Dialogue and change', textBody: 'Changing the narrative around how we talk about racial equity in systems and policies.' },
-    { key:'1', imageSrc: SpecLogo, header: ' Policy leadership', textBody: 'Preparing policy leaders and those that influence them to debate, design, and address racial equity going forward.' },
+    { key:'1', imageSrc: PillarOne, header: 'Methods and action', textBody: 'Identifying the most effective combination of approaches that can have enduring impact on systemic racism.' },
+    { key:'2', imageSrc: PillarTwo, header: 'Dialogue and change', textBody: 'Changing the narrative around how we talk about racial equity in systems and policies.' },
+    { key:'1', imageSrc: PillarThree, header: ' Policy leadership', textBody: 'Preparing policy leaders and those that influence them to debate, design, and address racial equity going forward.' },
   ]
 
   const specDataBody = specData.map(data => {
     return (
       <div className='pb-8 px-24' key={data.key}>
         <Image src={data.imageSrc}/>
-        <h3 className='font-Poppins pb-8'>{data.header}</h3>
+        <h3 className='font-Poppins pb-8 text-spec-turquoise-600'>{data.header}</h3>
         <p>{data.textBody}</p>
       </div>
     )
@@ -45,14 +35,14 @@ function OrgSection() {
     return(
       <div className='pb-8 px-24' key={data.key}>
         <Image src={data.imageSrc}/>
-        <h3 className='font-Poppins pb-8'>{data.header}</h3>
+        <h3 className='font-Poppins pb-8 text-spec-yellow-600'>{data.header}</h3>
         <p>{data.textBody}</p>
       </div>
     )
   })
 
   return (
-    <div className='w-screen h-[900px] bg-slate-400'>
+    <div className='w-screen h-[2600px] bg-spec-black'>
 
       <div>
         <div className='text-center p-12 font-Poppins'>
