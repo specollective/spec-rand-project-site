8b239998f7b99aacdeb443fb9623624d05e8e58c
Saige Cross <saigecrossdev@gmail.com>
2022-09-26 12:31:33 -0500

    WIP added a few meeple tickets, PR begun

--- pages/meeple.js
+++ pages/meeple.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import Subfooter from "../components/sub-footer";
+import Image from 'next/image'
+import MeepleMetaverse from '../public/MeepleMetaverse.svg'
 
 const MeeplePage = () => {
   return (
@@ -8,8 +10,17 @@ const MeeplePage = () => {
         <title>Meeple | Sustainable Progress Equality Collective</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Meeple</h1>
-      <Subfooter></Subfooter>
+
+      {/* <div>
+        BACK TO HOME LINK
+      </div> */}
+      
+      <div className="w-screen h-auto p-12 bg-spec-black-600 text-center">
+        <Image src={MeepleMetaverse} alt="meeple hero" />
+      </div>
+
+
+      <Subfooter />
     </section>
   );
 };
