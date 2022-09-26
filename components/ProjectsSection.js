713055c229d3878e70652b9c566feef66e0e04b8
Yoline <92892101+YolineB@users.noreply.github.com>
2022-09-26 12:17:00 -0700

    Adjust header for desktop and mobile

--- components/ProjectsSection.js
+++ components/ProjectsSection.js
@@ -1,4 +1,3 @@
-import SectionContent from './section-content'
 import SectionHeader from './section-header'
 import SubHeader from './sub-header'
 
@@ -6,16 +5,16 @@ function ProjectsSection() {
 
   return (
     <>
-      <SectionContent className="bg-spec-black-600">
-        <SectionHeader className="text-center"> Projects </SectionHeader>
-        <SubHeader className="justify-around items-center py-7"> 
-          CAREP Pillar I - Methods and Action
+      <div className="p-5 md:px-36 w-screen h-auto bg-spec-black-600">
+        <h1 className="text-4xl md:text-6xl font-bold py-4 text-center"> Projects </h1>
+        <section className="flex flex-col md:flex-row gap-y-3 py-4 text-spec-yellow-600 text-xl tracking-wide font-medium justify-around items-center"> 
+          <div>CAREP Pillar I - Methods and Action</div>
           <span className="hidden md:block rounded-full bg-spec-white-600 h-1.5 w-1.5"></span>
-          CAREP Pillar II - Dialogue and Change
+          <div>CAREP Pillar II - Dialogue and Change</div>
           <span className="hidden md:block rounded-full bg-spec-white-600 h-1.5 w-1.5"></span>
-          CAREP Pillar III - Policy Leadership
-        </SubHeader>
-      </SectionContent>
+          <div>CAREP Pillar III - Policy Leadership</div>
+        </section>
+      </div>
     </>
   )
 }
