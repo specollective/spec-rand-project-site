810aa29641237120b4a6ffdfe7d8e56b42b0169f
Yoline <92892101+YolineB@users.noreply.github.com>
2022-09-27 16:15:10 -0700

    Update imgaes, update project info

--- components/ProjectsSection.js
+++ components/ProjectsSection.js
@@ -1,7 +1,10 @@
 import Image from "next/image";
 import ProjectsAccordian from "./projects-accordian";
 
-import fakeImage from '../public/spec_logo.svg';
+import microcredSnap from '../public/snap_microcredential.svg';
+import meepleSnap from '../public/snap_meeple.svg';
+import digitalGallerySnap from '../public/snap_digital_gallery.svg';
+import joERSnap from '../public/snap_JoER.svg';
 
 function ProjectsSection() {
 
@@ -11,7 +14,7 @@ function ProjectsSection() {
       name: 'Microcredential Project',
       tag: 'Learning Management System',
       info: 'CAREP and SPEC have partnered together on this project with the aim of expanding access to education in the areas of diversity, equity, inclusion (DEI), mass atrocity prevention, game-based learning, and professional development. This project kicked off: November 2021 ',
-      pic: <Image src={fakeImage} alt='fake-image'/>,
+      pic: <Image src={microcredSnap} alt='mircocred-image'/>,
       linkLine:'View Project Details >>',
       link: '/micro-credential',
     }, 
@@ -19,8 +22,8 @@ function ProjectsSection() {
       id: 2,
       name: 'Gaming Project',
       tag: 'Socio-cultural game-based learning',
-      info: 'I AM INFO',
-      pic: <Image src={fakeImage} alt='fake-image'/>,
+      info: "The aim of this project, funded by RAND's Center to Advance Racial Equity Policy, is to engage and educate the public on topics related to racial equity policy through socio-cultural game-based learning.This project kicked off: December 2021",
+      pic: <Image src={meepleSnap} alt='meeple-image'/>,
       linkLine:'View Project Details >>',
       link: '/meeple',
     }, 
@@ -28,8 +31,8 @@ function ProjectsSection() {
       id: 3,
       name: 'Digital Expressions Gallery',
       tag: 'Digital Expressions of Mass Incarceration Online Gallery',
-      info: 'I AM INFO',
-      pic: <Image src={fakeImage} alt='fake-image'/>,
+      info: 'Incarceration can be a particularly traumatizing experience for LGBTQ+ individuals. Studies of prison art programs suggest that artistic activities have several benefits for prisoner rehabilitation and institutional management. This gallery of work by LGBTQ+ incarcerated artists was produced during the CAREP/SPEC partnership in collaboration with advocacy group ABO Comix.',
+      pic: <Image src={digitalGallerySnap} alt='digital-gallery-image'/>,
       linkLine:'Visit the Gallery >>',
       link: 'https://medium.com/journal-of-engaged-research/expressions/home',
     }, 
@@ -37,8 +40,8 @@ function ProjectsSection() {
       id: 4,
       name: 'Journal of Engaged Research',
       tag: 'Sharing Knowledge. Building Communities.',
-      info: 'I AM INFO',
-      pic: <Image src={fakeImage} alt='fake-image'/>,
+      info: 'The diversity of the publishing sector does not reflect that of the US population. The Journal of Engaged Research (JoER) elevates the perspectives of emerging scholars, students, artists and activists from historically marginalized groups. During the CAREP/SPEC partnership, collaborators from the Pardee RAND Graduate School, Wabash College and a CAREP Public Discourse Fellow worked with the co editors of JoER to publish articles.',
+      pic: <Image src={joERSnap} alt='JoER-image'/>,
       linkLine:'Visit the Journal >>',
       link: 'https://medium.com/journal-of-engaged-research',
     }, 
