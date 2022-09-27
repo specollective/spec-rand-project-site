a8bafa62057b3893a985095fdb40fc8c94f8dbd6
Saige Cross <saigecrossdev@gmail.com>
2022-09-27 14:56:34 -0500

    Added image. pretty much it.

--- pages/micro-credential.js
+++ pages/micro-credential.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import Image from 'next/image'
+import microHeaderImg from '../public/microcredential_dev_img.svg'
 import Subfooter from '../components/sub-footer'
 
 const MicroCredentialPage = () => {
@@ -8,7 +10,12 @@ const MicroCredentialPage = () => {
         <title>Microcredentials | Sustainable Progress Equality Collective</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Microcredential Page</h1>
+      <div className='bg-spec-black-600'>
+        <div className='px-20 text-center'>
+          <Image layout="responsive" src={microHeaderImg} />
+        </div>
+        <h1>Microcredential Page</h1>
+      </div>
       <Subfooter></Subfooter>
     </section>
 
