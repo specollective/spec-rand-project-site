fffa89ad67dbda97e38b4fe1d729340145184e5a
Saige Cross <saigecrossdev@gmail.com>
2022-09-27 08:20:50 -0500

    Projects added and everything is mapping correctly.

--- components/RAprofile.js
+++ components/RAprofile.js
@@ -5,8 +5,16 @@ import LinkedInRAs from '../public/linkedInRAs.svg'
 
 
 function RAprofile() {
+
+  const microCredentialProj = <a href='https://www.google.com/' className='text-spec-yellow-600'>Microcredential Project {'>>'}</a>
+  const gamingProj = <a href='https://www.google.com/' className='text-spec-yellow-600'>Gaming Project {'>>'}</a>
+  const impactProj = <a href='https://www.google.com/' className='text-spec-yellow-600'>Impact Website {'>>'}</a>
+  const expressionsProj = <a href='https://www.google.com/' className='text-spec-yellow-600'>Expressions Gallery {'>>'}</a>
+  const journalProj = <a href='https://www.google.com/' className='text-spec-yellow-600'>Journal of Engaged Research {'>>'}</a>
+
+  
   const profileData = [
-    { fullName: 'Alicia Bong', firstName: 'Alicia', title: 'Project Manager', pic: Alicia, alt: 'Alicia pic', linkedInLink: 'https://www.linkedin.com/in/alicia-bong-a4400896/', text: 'Alicia is a project manager focusing on team building and open communication. Her experience comes from multiple software adjacent roles in customer success and project management. She received her Bachelors Degree in English from SUNY Oneonta. Alicia presented at the first New Critics Undergraduate Literature and Composition Conference and was the Vice President of their chapter of the Sigma Tau Delta English Honors Society. Her interest in sustainability and social justice has led her to join the team at SPEC.', projects: ['Microcredential Project', 'Gaming Project','Impact Website'],}
+    { fullName: 'Alicia Bong', firstName: 'Alicia', title: 'Project Manager', pic: Alicia, alt: 'Alicia pic', linkedInLink: 'https://www.linkedin.com/in/alicia-bong-a4400896/', text: 'Alicia is a project manager focusing on team building and open communication. Her experience comes from multiple software adjacent roles in customer success and project management. She received her Bachelors Degree in English from SUNY Oneonta. Alicia presented at the first New Critics Undergraduate Literature and Composition Conference and was the Vice President of their chapter of the Sigma Tau Delta English Honors Society. Her interest in sustainability and social justice has led her to join the team at SPEC.', projects: [microCredentialProj, gamingProj, impactProj],}
   ]
 
   const profileDataMap = profileData.map(data => {
@@ -35,7 +43,7 @@ function RAprofile() {
             <p className='p-6'>{data.firstName} has worked on:</p>
 
             {data.projects.map((proj, index) => (
-              <p key={index} className='text-spec-yellow-600'>{proj} {'>>'}</p>
+              <div key={index} className='text-spec-yellow-600'>{proj}</div>
             ))}
           </div>
         </div>
