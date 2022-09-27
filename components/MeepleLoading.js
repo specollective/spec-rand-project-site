d983b499b7e788462bc22f850e4699f0cfe17107
Saige Cross <saigecrossdev@gmail.com>
2022-09-27 12:41:19 -0500

    Made Loading a seperate component.

---
+++ components/MeepleLoading.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import Image from 'next/image'
+import MeepleBlue from '../public/meeple_blue_character.svg'
+import MeepleGray from '../public/meeple_gray_character.svg'
+import MeepleCheck from '../public/meeple_checkmark.svg'
+import MeepleBullet from '../public/meeple_bullet.svg'
+
+function MeepleLoading() {
+  const meepleCheckmark = <div className="text-center inline-block pr-2"><Image width={20} height={20} src={MeepleCheck} /></div>
+  const meepleBullet = <div className="inline-block align-middle pr-2"><Image width={20} height={20} src={MeepleBullet} /></div>
+
+  const loadingColOne = ['Identify goals & objectives', 'Literature review on RAND research', 'Selection of game topic', 'Selection of game mechanic', 'Produced a development process']
+  const loadingColTwo = ['Game design direction ', 'Create moodboards for look and feel', 'Produce UI wireframes']
+  const loadingColThree = ['Final content approved', 'Alpha game delivered', 'Produce UI wireframes', 'Initial testing and responses']
+
+  const mappedColOne = loadingColOne.map((data, index) => {
+    return (
+      <p key={index} className="py-2">{meepleCheckmark}{data}</p>
+    )
+  })
+
+  const mappedColTwo = loadingColTwo.map((data, index) => {
+    return (
+      <p key={index} className="py-2">{meepleCheckmark}{data}</p>
+    )
+  })
+
+  const mappedColThree = loadingColThree.map((data, index) => {
+    return (
+      <p key={index} className="py-2">{meepleBullet}{data}</p>
+    )
+  })
+
+  return (
+    <div>
+      <div className="p-8 text-center text-spec-turquoise-600 text-5xl font-extrabold">
+        Loading...
+      </div>
+
+      <div className="md:px-36 lg:grid lg:grid-cols-3 pt-8">
+        <div>
+          <Image src={MeepleBlue} alt="meeple one" />
+          <h1 className="pt-4 text-2xl font-bold">Phase 1: Complete</h1>
+          <div className="lg:px-6 text-left">
+            <div className="justify-center p-6">
+              {mappedColOne}
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <Image src={MeepleGray} alt="meeple one" />
+          <h1 className="text-2xl font-bold pt-4">Phase 2</h1>
+          <div className="lg:px-6 text-left">
+            <div className="justify-center p-6 text-base">
+              {mappedColTwo}
+              <p className="py-2">{meepleBullet}Finalize Meeple artwork</p>
+              <p className="py-2">{meepleBullet}Content development</p>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <Image src={MeepleGray} alt="meeple one" />
+          <h1 className="text-2xl font-bold pt-4">Phase 3</h1>
+          <div className="lg:px-6 text-left">
+            <div className="justify-center p-6 text-base">
+              {mappedColThree}
+              <p className="py-2 text-xl font-bold">{meepleBullet}GAME ONE LAUNCH!</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MeepleLoading
--- pages/meeple.js
+++ pages/meeple.js
@@ -2,15 +2,9 @@ import Head from "next/head";
 import Image from 'next/image'
 import MeepleMetaverse from '../public/MeepleMetaverse.svg'
 import Meeples from '../public/meeples.svg'
-import MeepleBlue from '../public/meeple_blue_character.svg'
-import MeepleGray from '../public/meeple_gray_character.svg'
-import MeepleCheck from '../public/meeple_checkmark.svg'
-import MeepleBullet from '../public/meeple_bullet.svg'
+import MeepleLoading from "../components/MeepleLoading"
 
 const MeeplePage = () => {
-  const meepleCheckmark = <div className="text-center inline-block pr-2"><Image width={20} height={20} src={MeepleCheck} /></div>
-  const meepleBullet = <div className="inline-block align-middle pr-2"><Image width={20} height={20} src={MeepleBullet} /></div>
-
   return (
     <section>
       <Head>
@@ -45,55 +39,7 @@ const MeeplePage = () => {
             <p>• To learn about policy solutions/recommendation that improve racial equity</p>
           </div>
         </div>
-
-        <div>
-          <div className="p-8 text-center text-spec-turquoise-600 text-5xl font-extrabold">
-            Loading...
-          </div>
-
-          <div className="md:px-36 lg:grid lg:grid-cols-3 pt-8">
-            <div>
-              <Image src={MeepleBlue} alt="meeple one" />
-              <h1 className="pt-4 text-2xl font-bold">Phase 1: Complete</h1>
-              <div className="lg:px-6 text-left">
-                <div className="justify-center p-6">
-                  <p className="py-2">{meepleCheckmark}Identify goals & objectives</p>
-                  <p className="py-2">{meepleCheckmark}Literature review on RAND research</p>
-                  <p className="py-2">{meepleCheckmark}Selection of game topic</p>
-                  <p className="py-2">{meepleCheckmark}Selection of game mechanic</p>
-                  <p className="py-2">{meepleCheckmark}Produced a development process</p>
-                </div>
-              </div>
-            </div>
-
-            <div>
-              <Image src={MeepleGray} alt="meeple one" />
-              <h1 className="text-2xl font-bold pt-4">Phase 2</h1>
-              <div className="lg:px-6 text-left">
-                <div className="justify-center p-6 text-base">
-                  <p className="py-2">{meepleCheckmark}Game design direction </p>
-                  <p className="py-2">{meepleCheckmark}Create moodboards for look and feel</p>
-                  <p className="py-2">{meepleCheckmark}Produce UI wireframes</p>
-                  <p className="py-2">{meepleBullet}Finalize Meeple artwork</p>
-                  <p className="py-2">{meepleBullet}Content development</p>
-                </div>
-              </div>
-            </div>
-
-            <div>
-              <Image src={MeepleGray} alt="meeple one" />
-              <h1 className="text-2xl font-bold pt-4">Phase 3</h1>
-              <div className="lg:px-6 text-left">
-                <div className="justify-center p-6 text-base">
-                  <p className="py-2">{meepleBullet}Game design direction </p>
-                  <p className="py-2">{meepleBullet}Create moodboards for look and feel</p>
-                  <p className="py-2">{meepleBullet}Produce UI wireframes</p>
-                  <p className="py-2 text-xl font-bold">{meepleBullet}GAME ONE LAUNCH!</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <MeepleLoading />
 
       </div>
     </section>
