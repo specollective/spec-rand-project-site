47b477758453efc4e8931b2de44fec6a91b8c38f
Saige Cross <saigecrossdev@gmail.com>
2022-09-28 16:13:52 -0500

    Fixed double h1 issue per Jack

--- pages/research-assistants.js
+++ pages/research-assistants.js
@@ -13,7 +13,7 @@ const ResearchAssistantPage = () => {
         <div className='md:flex md:space-x-24 lg:px-60'>
           <div className='text-center md:text-left'>
             <h1 className='text-[70px] text-spec-yellow-600  font-extrabold'>Meet <br />the<br /></h1>
-            <h1 className='text-[85px] text-spec-turquoise-600 font-bold'>RAs</h1>
+            <span className='text-[85px] text-spec-turquoise-600 font-bold'>RAs</span>
           </div>
           <div className='inline-block h-auto px-8 md:px-0 md:pt-40 md:text-lg'>
             <p className=''>The SPEC / CAREP Research associates used open collaboration throughout the project-building process and are excited to continue to share knowledge, and resources, and collaboratively innovate to create systemic change.</p>
@@ -27,8 +27,6 @@ const ResearchAssistantPage = () => {
         <div>
           <RAprofile />
         </div>
-
-
       </section>
     </>
   )
