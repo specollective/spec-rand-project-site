dbdbac3e8afc22d033441b8953c089d1888f45b6
Saige Cross <saigecrossdev@gmail.com>
2022-09-28 15:47:26 -0500

    Adding merge changes

--- pages/research-assistants.js
+++ pages/research-assistants.js
@@ -1,9 +1,6 @@
 import Head from 'next/head'
-<<<<<<< HEAD
 import RAprofile from '../components/RAprofile'
-=======
 import Dropdown from '../components/RAsDropdown'
->>>>>>> 561c7f0 (Almost finished, finalizing styles)
 
 const ResearchAssistantPage = () => {
   return (
@@ -13,7 +10,6 @@ const ResearchAssistantPage = () => {
         <title>Research Assistant | Sustainable Progress Equality Collective</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-<<<<<<< HEAD
       <div className='bg-spec-black-600'>
         <h1 className='text-spec-black-600'>RA Page</h1>
       </div>
@@ -23,20 +19,18 @@ const ResearchAssistantPage = () => {
       </div>
 
 
-=======
->>>>>>> 561c7f0 (Almost finished, finalizing styles)
     </section>
-    <section className='m-auto p-6 md:w-2/3 md:p-20 bg-spec-black-600'>
-      <div className='md:flex md:space-x-24'>
-        <div className='text-center md:text-left'>
-          <h1 className='text-[50px] md:text-[70px] text-spec-yellow-600 font-extrabold'>Meet <br />the<br /></h1>
-          <h1 className='text-[65px] md:text-[85px] text-spec-turquoise-600 font-bold'>RAs</h1>
+    <section className='w-screen h-auto p-20 bg-spec-black-600'>
+      <div className='flex space-x-24 lg:px-60'>
+        <div>
+          <h1 className='text-[70px] text-spec-yellow-600  font-extrabold'>Meet <br />the<br /></h1>
+          <h1 className='text-[85px] text-spec-turquoise-600 font-bold'>RAs</h1>
         </div>
-        <div className='inline-block h-auto px-8 md:px-0 md:pt-40 md:text-lg '>
-          <p>The SPEC / CAREP Research associates used open collaboration throughout the project-building process and are excited to continue to share knowledge, and resources, and collaboratively innovate to create systemic change.</p>
+        <div className='inline-block pt-40 h-auto text-lg'>
+          <p className=''>The SPEC / CAREP Research associates used open collaboration throughout the project-building process and are excited to continue to share knowledge, and resources, and collaboratively innovate to create systemic change.</p>
         </div>
       </div>
-      <div className='text-center md:text-left'>
+      <div className='w-2/3 m-auto'>
         <Dropdown />
       </div>
     </section>
