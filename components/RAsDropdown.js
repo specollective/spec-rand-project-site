5aeb09ac55bbe63a31f455043ca07ca36e404439
Saige Cross <saigecrossdev@gmail.com>
2022-09-28 09:59:35 -0500

    fixed styles, now mobile

--- components/RAsDropdown.js
+++ components/RAsDropdown.js
@@ -9,7 +9,7 @@ function RAsDropdown() {
       <div>
         <button type="button" onClick={() => setMenuToggle(!menuToggle)} className="inline-flex w-full justify-center rounded-lg border-spec-yellow-600 border-2 px-4 py-2 text-sm font-medium text-white shadow-sm focus:outline-none focus:ring-spec-yellow-600" id="menu-button" aria-expanded="true" aria-haspopup="true">
           Jump To
-          <div className='pl-20'>
+          <div className='pl-24'>
             <svg class="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
               <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
             </svg>
