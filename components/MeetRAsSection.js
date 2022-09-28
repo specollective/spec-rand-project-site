e3aed5f7f14fe5073c3637e2b7e4ae8ad21fdff5
Yoline <92892101+YolineB@users.noreply.github.com>
2022-09-27 21:12:01 -0700

    Add mobile view headers and content

--- components/MeetRAsSection.js
+++ components/MeetRAsSection.js
@@ -1,7 +1,23 @@
-export default function MeetRAs () {
+export default function MeetRAs() {
   return (
-    <div className='w-screen h-auto bg-spec-teal-600'>
-      
+    <div className='w-screen h-auto bg-spec-teal-600 px-5 md:px-36 py-20 text-Poppins'>
+      <section className='flex flex-col md:flex-col-2 md:flex-row'>
+        <h1 className='text-4xl md:text-6xl font-bold leading-relaxed'>
+          SPEC / CAREP Research Associates (RA)
+        </h1>
+        <h2 className='text-base md:text-xl text-spec-yellow-600 py-5 leading-relaxed'>
+          CAREP Pillar III - Policy Leadership
+        </h2>
+        <p className='text-base md:text-lg tracking-wide'>
+          During the 2021-2022 Fiscal Year, SPEC / CAREP research associates, 
+          project staff and collaborators worked together throughout the 
+          project-building process. All are excited to continue to share knowledge, 
+          and resources, and collaboratively innovate to create systemic change in 
+          their communities. Funding for this research was provided by unrestricted 
+          gifts from RAND supporters and income from operations, with which RAND 
+          established the Center to Advance Racial Equity Policy in 2020.
+        </p>
+      </section>
     </div>
   )
 };
