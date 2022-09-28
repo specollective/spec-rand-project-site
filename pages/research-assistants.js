a6d6c0ea1d29704d1385ffae45a8ee8dc0e87341
Saige Cross <saigecrossdev@gmail.com>
2022-09-28 16:19:24 -0500

    NOW fixed Jacks requested changes.

--- pages/research-assistants.js
+++ pages/research-assistants.js
@@ -12,8 +12,11 @@ const ResearchAssistantPage = () => {
       <section className='w-screen h-auto md:p-20 bg-spec-black-600'>
         <div className='md:flex md:space-x-24 lg:px-60'>
           <div className='text-center md:text-left'>
-            <h1 className='text-[70px] text-spec-yellow-600  font-extrabold'>Meet <br />the<br /></h1>
-            <span className='text-[85px] text-spec-turquoise-600 font-bold'>RAs</span>
+          <h1 className='text-[50px] md:text-[70px] text-spec-yellow-600 font-extrabold'>
+            Meet
+            <br />the<br />
+            <span className='text-[65px] md:text-[85px] text-spec-turquoise-600 font-bold'>RAs</span>
+          </h1>
           </div>
           <div className='inline-block h-auto px-8 md:px-0 md:pt-40 md:text-lg'>
             <p className=''>The SPEC / CAREP Research associates used open collaboration throughout the project-building process and are excited to continue to share knowledge, and resources, and collaboratively innovate to create systemic change.</p>
