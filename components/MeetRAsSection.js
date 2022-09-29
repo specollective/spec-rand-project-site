4507d299e9818a3dd21656b3d4d65232e3eb0fb8
Yoline <92892101+YolineB@users.noreply.github.com>
2022-09-29 10:42:07 -0700

    Add RA squares, adjust heading grid size

--- components/MeetRAsSection.js
+++ components/MeetRAsSection.js
@@ -1,14 +1,16 @@
+import RaSquares from "./RaSquares"
+
 export default function MeetRAs() {
   return (
     <div className='xs:px-5 md:px-36 lg:px-40 w-screen h-auto bg-spec-teal-600 py-20 text-Poppins'>
-      <section className='md:grid grid-rows-2 grid-cols-2 gap-x-32'>
+      <section className='md:grid grid-rows-2 grid-cols-2 auto-rows-min px-20'>
         <h1 className='text-4xl md:text-6xl row-start-1 font-bold leading-relaxed pr-8'>
           SPEC / CAREP <br/> Research Associates (RA)
         </h1>
         <h2 className='xs:text-base md:text-xl row-start-2 text-spec-yellow-600 py-5 leading-relaxed'>
           CAREP Pillar III - Policy Leadership
         </h2>
-        <p className='text-base md:text-lg tracking-wide col-start-2'>
+        <p className='row-span-2 text-base md:text-lg tracking-wide'>
           During the 2021-2022 Fiscal Year, SPEC / CAREP research associates, 
           project staff and collaborators worked together throughout the 
           project-building process. All are excited to continue to share knowledge, 
@@ -18,9 +20,7 @@ export default function MeetRAs() {
           established the Center to Advance Racial Equity Policy in 2020.
         </p>
       </section>
-
-      {/* IMPLEMENT RAs GRID IMAGES */}
-
+      <RaSquares />
     </div>
   )
 };
