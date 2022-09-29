ab37200ff469a5f015efb2735108290c329fa226
Daniel <daniel.ashcraft@ofashandfire.com>
2022-09-29 17:06:27 -0500

    quick cleanup

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,7 @@ import ProjectsSection from "../components/ProjectsSection";
 import MeetRAs from "../components/MeetRAsSection";
 import ByTheNumbers from "../components/byTheNumbers";
 import ReflectionCarousel from "../components/reflection-carousel";
-import ScrollButton from "../components/scroll-button";
 import { microcredentialsObjList } from "../components/content/carouselContent";
-import { profileData } from "../components/content/profileContent";
-import RaSquares from '../components/RaSquares';
 
 export default function Home() {
   return (
@@ -23,9 +20,6 @@ export default function Home() {
       <ResearchArticle />
       <ProjectsSection />
       <MeetRAs />
-      <div className="bg-spec-teal-600 xs:p-6 md:px-24 md:py-12">
-        <RaSquares profileData={profileData}/>
-      </div>
       <ByTheNumbers />
       <section className="bg-spec-teal-600 flex mt-6 px-8 py-12 justify-center">
         <ReflectionCarousel
