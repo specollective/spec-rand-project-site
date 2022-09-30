e13573caf52d0b06db1503e3fd61668cd71d690b
Victoria <victoriakarenlo@gmail.com>
2022-09-30 15:17:17 -0700

    Add reflection to homepage

--- components/content/carouselContent.js
+++ components/content/carouselContent.js
@@ -1,5 +1,6 @@
 import yoline from "../../public/yoline_sm_bubble.svg";
 import crystal from "../../public/crystal_sm_bubble.svg";
+import victoria from "../../public/victoria_sm_bubble.svg";
 
 export const reflectionObjList = [
     {
@@ -17,10 +18,10 @@ export const reflectionObjList = [
       title: "Research Associate",
     },
     {
-      name: "Yoline Banerjee",
-      image: yoline,
+      name: "Victoria Lo",
+      image: victoria,
       quote:
-        "Sagittis viverra integer et mauris, sapien enim. Eget quis congue suspendisse elit. Nisi bibendum nisl diam nulla velit, magna eget tincidunt ornare. Et integer rhoncus, magnis quisque ut sollicitudin quam mauris aenean.",
+        "Working on the SPEC team on multiple RAND projects has given me so much insight into being a better developer and advocate for social and racial equity. As a junior developer, I was eager to quickly start building systems with software and learn as much as possible. I was able to build out microcredential courses on an open-source learn management system and contribute to building the impact website with Of Ash and Fire. I quickly understood the importance of mindfulness of unconscious biases and how to build systems that are inclusive to multiple end-users. I also enjoyed having regular meetings with Dr. Rhianna Rogers and other Research Associates to have an open conversation about our perspectives.",
       title: "Research Associate",
     },
     {
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,32 @@
-import Head from 'next/head';
-import ByTheNumbers from '../components/byTheNumbers';
-import OrgSection from '../components/OrgSection';
-import ProjectsSection from '../components/ProjectsSection';
-import RaSquares from '../components/RaSquares';
-import ReflectionCarousel from '../components/reflection-carousel';
-import ResearchArticle from '../components/researchArticle';
-import ScrollButton from '../components/scroll-button';
-import TopHeroComponent from '../components/TopHeroComponent';
-import { microcredentialsObjList } from '../components/content/carouselContent';
-import { profileData } from '../components/content/profileContent';
+import Head from "next/head";
+import TopHeroComponent from "../components/TopHeroComponent";
+import OrgSection from "../components/OrgSection";
+import ResearchArticle from "../components/researchArticle";
+import ProjectsSection from "../components/ProjectsSection";
+import MeetRAs from "../components/MeetRAsSection";
+import ByTheNumbers from "../components/byTheNumbers";
+import ReflectionCarousel from "../components/reflection-carousel";
+import { reflectionObjList } from "../components/content/carouselContent";
 
 export default function Home() {
-	return (
-		<section className="bg-spec-blue-600">
-			<Head>
-				<title>Sustainable Progress Equality Collective</title>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-			<TopHeroComponent />
-			<OrgSection />
-			<ResearchArticle />
-			<ProjectsSection />
-			<div className="bg-spec-teal-600 xs:p-6 md:px-24 md:py-12">
-				<RaSquares profileData={profileData} />
-			</div>
-			<ByTheNumbers />
-			<section className="bg-spec-teal-600 flex mt-6 px-8 py-12 justify-center">
-				<ReflectionCarousel reflectionObjList={microcredentialsObjList} borderColor="spec-yellow-600" />
-			</section>
-			<ScrollButton />
-		</section>
-	);
+  return (
+    <section className="bg-spec-blue-600">
+      <Head>
+        <title>Sustainable Progress Equality Collective</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <TopHeroComponent />
+      <OrgSection />
+      <ResearchArticle />
+      <ProjectsSection />
+      <MeetRAs />
+      <ByTheNumbers />
+      <section className="bg-spec-teal-600 flex mt-6 px-8 py-12 justify-center">
+        <ReflectionCarousel
+          reflectionObjList={reflectionObjList}
+          borderColor="spec-yellow-600"
+        />
+      </section>
+    </section>
+  );
 }
