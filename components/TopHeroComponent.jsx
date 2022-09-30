eace9a866992608644b3dea35971b9dae26fe2d5
Victoria <victoriakarenlo@gmail.com>
2022-09-30 14:59:27 -0700

    Replace both sides with images

--- components/TopHeroComponent.jsx
+++ components/TopHeroComponent.jsx
@@ -1,37 +1,16 @@
 import React from 'react'
 import Image from 'next/image'
-import SpecLogo from "../public/spec_logo.svg";
-import CAREP_logo from "../public/homepage_hero_assets/CAREP_logo.svg";
-import collab from "../public/homepage_hero_assets/collab.png";
+import hero_right from "../public/homepage_hero_assets/HeroRight.png";
+import hero_left from "../public/homepage_hero_assets/HeroLeft.png";
 
 function TopHeroComponent() {
   return (
-    <div className="grid grid-cols-2">
-
-      <div className="">
-        <div className="flex px-20">
-          <Image src={SpecLogo} alt="spec logo" width={180} height={180} />
-          <h1 className="justify-items-center text-5xl text-spec-white-600 font-Poppins md:pr-24">Sustainable Progress & Equality Collective </h1>
-        </div>
-
-        <div className="text-center text-2xl">
-          <Image src={collab} alt="collab image" width={200} height={80} />
-        </div>
-
-        <div className="flex px-20">
-          <h2 className="text-5xl text-spec-yellow-600 font-Lora pl-32 pr-1">
-            Center to Advance Racial Equity Policy
-          </h2>
-          <Image src={CAREP_logo} alt="center for racial equity logo" width={150} height={150} />
-        </div>
-        <div className="text-right mr-48 mt-2 text-lg">
-          <p>@ RAND</p>
-        </div>
-
+    <div className="md:grid md:grid-cols-2 my-2 md:my-0">
+      <div className="cols-span-1">
+        <Image src={hero_left} alt="hero left side"/>
       </div>
-
-      <div>
-        
+      <div className="cols-span-1">
+        <Image src={hero_right} alt="hero right side"/>
       </div>
     </div>
   )
