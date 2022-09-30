6da6db7a425088412e95a06aa9d79af7d6c96520
Victoria <victoriakarenlo@gmail.com>
2022-09-30 14:44:10 -0700

    Add first half

--- components/TopHeroComponent.jsx
+++ components/TopHeroComponent.jsx
@@ -1,39 +1,40 @@
-import React from 'react';
-import Image from 'next/image';
-import SpecCollab from '../public/SPECcollab.svg';
-import RAND from '../public/@RAND.svg';
+import React from 'react'
+import Image from 'next/image'
+import SpecLogo from "../public/spec_logo.svg";
+import CAREP_logo from "../public/homepage_hero_assets/CAREP_logo.svg";
+import collab from "../public/homepage_hero_assets/collab.png";
 
 function TopHeroComponent() {
-	return (
-		<div className="bg-hero-img bg-cover h-auto md:pt-4 lg:pt-10 bg-center text-center font-Poppins lg:pb-32 p-10 md:p-20">
-			<div className="xs:px-10 xs:pt-10 lg:px-20 md:w-4/5 lg:w-3/4 xl:w-2/3 2xl:w-1/2 md:m-auto">
-				<div>
-					<Image className="object-scale-down" src={SpecCollab} alt="logo" />
-				</div>
-				<div className="grid grid-cols-4">
-					<h2 className="col-span-4 xs:text-2xl lg:text-5xl font-bold text-white">
-						Center to Advance Racial Equity Policy{' '}
-					</h2>
-					<div className="col-start-4 xl:mt-3 xl:text-left">
-						<Image className="object-scale-down" src={RAND} alt="randLogo" />
-					</div>
-				</div>
-				<div className="mt-8 xs:pb-4 md:text-xl lg:pb-8 text-white">
-					<p>
-						In February 2022, Sustainable Progress and Equality Collective (SPEC) announced a partnership with the RAND
-						Center to Advance Racial Equity Policy to create and implement CAREP’s flagship Microcredential program.{' '}
-					</p>
-				</div>
-				<div className="md:text-xl text-white">
-					<p>
-						A special class of SPEC/RAND Research Associates was comprised of a diverse group of individuals with
-						experience in academic, healthcare, corporate, government, and nonprofit settings to gain in-depth
-						experience in research, educational technology, and organizational development.
-					</p>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="grid grid-cols-2">
+
+      <div className="">
+        <div className="flex px-20">
+          <Image src={SpecLogo} alt="spec logo" width={180} height={180} />
+          <h1 className="justify-items-center text-5xl text-spec-white-600 font-Poppins md:pr-24">Sustainable Progress & Equality Collective </h1>
+        </div>
+
+        <div className="text-center text-2xl">
+          <Image src={collab} alt="collab image" width={200} height={80} />
+        </div>
+
+        <div className="flex px-20">
+          <h2 className="text-5xl text-spec-yellow-600 font-Lora pl-32 pr-1">
+            Center to Advance Racial Equity Policy
+          </h2>
+          <Image src={CAREP_logo} alt="center for racial equity logo" width={150} height={150} />
+        </div>
+        <div className="text-right mr-48 mt-2 text-lg">
+          <p>@ RAND</p>
+        </div>
+
+      </div>
+
+      <div>
+        
+      </div>
+    </div>
+  )
 }
 
 export default TopHeroComponent;
