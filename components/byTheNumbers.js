f24e5654aeef06d331708665cccb8f5644b35d0c
Jack Kinsey <jack.w.kinsey@gmail.com>
2022-09-30 12:42:47 -0400

    chore: update styles for stats for large screens

--- components/byTheNumbers.js
+++ components/byTheNumbers.js
@@ -2,35 +2,56 @@ import StatBox from './stat-box';
 
 export default function ByTheNumbers() {
 	return (
-		<div className="grid grid-cols-1 md:grid-cols-2 md:col-span-2 align-items-center justify-items-center bg-spec-black-600 mt-4 p-10 md:p-20">
-			<div className="cols-span-1 md:justify-self-end md:pr-4">
-				<div className="text-4xl font-bold text-spec-white-600 mb-5">
-					<h1 className="text-center md:text-left mb-5 md:w-24">By the Numbers</h1>
-				</div>
-				<StatBox
-					borderColor="spec-turquoise-600"
-					height="h-96"
-					width="w-80"
-					statColor="spec-yellow-600"
-					inputStat="90%"
-				>
-					<p>
-						of individuals inside the organization come from diverse backgrounds; that might include visible/invisible
-						diabilities, LGBTQ+, women, people of color.
-					</p>
-				</StatBox>
-			</div>
-			<div className="cols-span-1 md:justify-self-start md:pl-4">
-				<StatBox borderColor="spec-turquoise-600" height="h-48" width="w-96" statColor="spec-yellow-600" inputStat="1">
-					<p>Research Article Published</p>
-				</StatBox>
-				<StatBox borderColor="spec-turquoise-600" height="h-48" width="w-96" statColor="spec-yellow-600" inputStat="12">
-					<p>Research Associates (RAs) went through this program</p>
-				</StatBox>
-				<StatBox borderColor="spec-turquoise-600" height="h-48" width="w-96" statColor="spec-yellow-600" inputStat="6">
-					<p>Research Associates (RAs) placed at jobs; externally or through SPEC</p>
-				</StatBox>
-			</div>
+		<div className="grid grid-cols-1 md:grid-cols-2 md:grid-rows-3 bg-spec-black-600 md:justify-items-center items-center p-10 md:p-20 md:w-4/5 xl:w-1/2 md:m-auto">
+			<h2 className="text-5xl font-bold text-spec-white-600 text-left">
+				By the
+				<br />
+				Numbers
+			</h2>
+
+			<StatBox
+				className="row-start-2 col-start-1 row-span-2 my-4 md:my-0"
+				borderColor="spec-turquoise-600"
+				height="h-96"
+				width="w-80"
+				statColor="spec-yellow-600"
+				inputStat="90%"
+			>
+				<p>
+					of individuals inside the organization come from diverse backgrounds; that might include visible/invisible
+					diabilities, LGBTQ+, women, people of color.
+				</p>
+			</StatBox>
+			<StatBox
+				className="my-4 md:my-0"
+				borderColor="spec-turquoise-600"
+				height="h-48"
+				width="w-80 md:w-96"
+				statColor="spec-yellow-600"
+				inputStat="1"
+			>
+				<p>Research Article Published</p>
+			</StatBox>
+			<StatBox
+				className="my-4 md:my-0"
+				borderColor="spec-turquoise-600"
+				height="h-48"
+				width="w-80 md:w-96"
+				statColor="spec-yellow-600"
+				inputStat="12"
+			>
+				<p>Research Associates (RAs) went through this program</p>
+			</StatBox>
+			<StatBox
+				className="my-4 md:my-0"
+				borderColor="spec-turquoise-600"
+				height="h-48"
+				width="w-80 md:w-96"
+				statColor="spec-yellow-600"
+				inputStat="6"
+			>
+				<p>Research Associates (RAs) placed at jobs; externally or through SPEC</p>
+			</StatBox>
 		</div>
 	);
 }
