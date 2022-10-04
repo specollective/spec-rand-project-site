84acbe56f8b0fa6828c619d4e9b38e24008d1259
Yoline <92892101+YolineB@users.noreply.github.com>
2022-10-04 15:04:25 -0700

    Add Yoline's reflection quote

--- components/content/carouselContent.js
+++ components/content/carouselContent.js
@@ -6,7 +6,7 @@ export const reflectionObjList = [
       name: "Yoline Banerjee",
       image: yoline,
       quote:
-        "Sagittis viverra integer et mauris, sapien enim. Eget quis congue suspendisse elit. Nisi bibendum nisl diam nulla velit, magna eget tincidunt ornare. Et integer rhoncus, magnis quisque ut sollicitudin quam mauris aenean.",
+        "The partnership between RAND and SPEC has allowed me to gain valuable software development experience in an enriching, collaborative, and inclusive space. Our projects involved building different applications and systems with popular tech stacks, providing me with a better tool set to enter the tech sector. I was encouraged to ask questions, try different solutions, and connect with people that delivered additional resources and ideas. What once made me feel like a solo climber facing a daunting mountain soon transformed into a fantastic and supportive core team ready to help everyone ascend to the top. I now feel confident stepping into a sector I previously had minimal experience in, with the ability always to make a conscious effort to provide a meaningful impact in all communities I am a part of.",
       title: "Research Associate",
     },
     {
